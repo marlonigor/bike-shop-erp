8381783e846ba8fcaae096f88b9d277d546316af
Igor Marlon <igormarlon.o@gmail.com>
2025-12-13 01:55:54 -0300

    feat(catalog): implement Product model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Brand
+from .models import Category, Brand, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,3 +11,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at')
     search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'name', 'category', 'brand', 'price', 'is_service', 'active')
+    list_filter = ('category', 'brand', 'active', 'is_service')
+    search_fields = ('name', 'sku')
+    list_editable = ('price', 'active') # Permite editar preço direto na lista!
+    autocomplete_fields = ['category', 'brand'] # Útil quando tivermos muitas marcas
--- catalog/models.py
+++ catalog/models.py
@@ -24,6 +24,7 @@ class Category(ModelBase):
         return f"{self.name} ({self.get_type_display()})"
 
 class Brand(ModelBase):
+
     name = models.CharField(max_length=100, verbose_name="Nome")
 
     class Meta:
@@ -33,3 +34,60 @@ class Brand(ModelBase):
 
     def __str__(self):
         return self.name
+
+class Product(ModelBase):
+    name = models.CharField(max_length=120, verbose_name="Nome")
+    sku = models.CharField(
+        max_length=50, 
+        unique=True, 
+        verbose_name="SKU (Código)"
+    )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.PROTECT, 
+        related_name='products',
+        verbose_name="Categoria"
+    )
+    brand = models.ForeignKey(
+        Brand, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name='products',
+        verbose_name="Marca"
+    )
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        verbose_name="Preço de Venda"
+    )
+    cost = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        default=0, 
+        verbose_name="Preço de Custo"
+    )
+    is_service = models.BooleanField(
+        default=False, 
+        verbose_name="É serviço?"
+    )
+    active = models.BooleanField(
+        default=True, 
+        verbose_name="Ativo"
+    )
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.sku} - {self.name}"
+
+    def save(self, *args, **kwargs):
+        # Automatização: Se a categoria for SERVIÇO, marca o produto como serviço automaticamente
+        if self.category and self.category.type == Category.Type.SERVICE:
+            self.is_service = True
+        else:
+            self.is_service = False
+        super().save(*args, **kwargs)
